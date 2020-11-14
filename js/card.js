2f27bceb2c64da67dc8345b6c36ea5f86d0a13fd
EnjoyTWOS <danylyuk.v@gmail.com>
2020-11-14 06:39:26 +0200

    Update card.js

--- js/card.js
+++ js/card.js
@@ -22,7 +22,7 @@
     return mapAvatarArray;
   };
 
-  const renderPhotos = (card, mapCard) => {
+  const createPhotosFragment = (card, mapCard) => {
     mapCard.querySelector(`.popup__photo`).setAttribute(`src`, card.offer.photos[0]);
     for (let i = 1; i < card.offer.photos.length; i++) {
       const photosMapCard = mapCard.querySelector(`.popup__photo`).cloneNode(true);
@@ -81,13 +81,18 @@
       mapCard.querySelector(`.popup__features`).appendChild(createFeatureFragment(card));
       mapCard.querySelector(`.popup__text--price`).textContent = card.offer.price + `₽/ночь`;
       mapCard.querySelector(`.popup__type`).textContent = MAP_ACCOMODATION_LIST[card.offer.type];
-      mapCard.querySelector(`.popup__text--capacity`).textContent = card.offer.rooms + ` комнаты для ` + card.offer.guests + ` гостей`;
       mapCard.querySelector(`.popup__text--time`).textContent = `Заезд после ` + card.offer.checkin + `, выезд до ` + card.offer.checkout;
       mapCard.querySelector(`.popup__description`).textContent = card.offer.description;
       mapCard.querySelector(`.popup__avatar`).setAttribute(`src`, card.author.avatar);
 
+      if (card.offer.rooms === 0 || card.offer.guests === 0) {
+        mapCard.querySelector(`.popup__text--capacity`).classList.add(`visually-hidden`);
+      } else {
+        mapCard.querySelector(`.popup__text--capacity`).textContent = card.offer.rooms + ` комнаты для ` + card.offer.guests + ` гостей`;
+      }
+
       if (card.offer.photos.length > 0) {
-        renderPhotos(card, mapCard);
+        createPhotosFragment(card, mapCard);
       } else {
         mapCard.querySelector(`.popup__photo`).classList.add(`visually-hidden`);
       }
